a975d9209a0f5072be7d52a13e4290e45ce841cb
osawa-koki <github@osawa.dev>
2022-12-30 22:45:25 +0900

    小さな修正。

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -5,9 +5,12 @@ const AboutPage = () => (
   <Layout title="Nextjsテンプレートについて。">
     <h1>About</h1>
     <p>This is the about page</p>
-    <p>
-      <Link href="/">Go home</Link>
-    </p>
+    <ul>
+      <li>api/{name} (GET)</li>
+      <li>api/{name} (POST)</li>
+      <li>api/{name} (PUT)</li>
+      <li>api/{name} (DELETE)</li>
+    </ul>
   </Layout>
 )
 
