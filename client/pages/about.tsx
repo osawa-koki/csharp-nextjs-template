e16b65b74a3c8c212785930a60f17fb40937b78d
osawa-koki <github@osawa.dev>
2022-12-30 22:49:42 +0900

    tsx記法と衝突していた、、、

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 const AboutPage = () => (
   <Layout title="Nextjsテンプレートについて。">
     <h1>About</h1>
     <p>This is the about page</p>
     <ul>
-      <li>api/{name} (GET)</li>
-      <li>api/{name} (POST)</li>
-      <li>api/{name} (PUT)</li>
-      <li>api/{name} (DELETE)</li>
+      <li>/api/name (GET)</li>
+      <li>/api/name (POST)</li>
+      <li>/api/name (PUT)</li>
+      <li>/api/name (DELETE)</li>
     </ul>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
